43abb04c6c696de5b53c5377b84d9b2e367e43fa
jerry73204 <jerry73204@gmail.com>
2020-11-17 04:15:42 +0800

    Add common activation functions

--- tch-goodies/src/lib.rs
+++ tch-goodies/src/lib.rs
@@ -1,7 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use tch::Tensor;
+
+pub trait TensorExt {
+    fn swish(&self) -> Tensor;
+    fn hard_swish(&self) -> Tensor;
+    fn mish(&self) -> Tensor;
+}
+
+impl TensorExt for Tensor {
+    fn swish(&self) -> Tensor {
+        self * self.sigmoid()
+    }
+
+    fn hard_swish(&self) -> Tensor {
+        self * (self + 3.0).clamp(0.0, 6.0) / 6.0
+    }
+
+    fn mish(&self) -> Tensor {
+        self * &self.softplus().tanh()
     }
 }
