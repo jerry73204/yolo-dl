79c54569678605739185f9bd252b24382809c093
jerry73204 <jerry73204@gmail.com>
2020-11-17 04:28:57 +0800

    Implement hard_mish activation

--- tch-goodies/src/lib.rs
+++ tch-goodies/src/lib.rs
@@ -4,6 +4,9 @@ pub trait TensorExt {
     fn swish(&self) -> Tensor;
     fn hard_swish(&self) -> Tensor;
     fn mish(&self) -> Tensor;
+    fn hard_mish(&self) -> Tensor;
+    // fn normalize_channels(&self) -> Tensor;
+    // fn normalize_channels_softmax(&self) -> Tensor;
 }
 
 impl TensorExt for Tensor {
@@ -18,4 +21,18 @@ impl TensorExt for Tensor {
     fn mish(&self) -> Tensor {
         self * &self.softplus().tanh()
     }
+
+    fn hard_mish(&self) -> Tensor {
+        let case1 = self.clamp(-2.0, 0.0);
+        let case2 = self.clamp_min(0.0);
+        (case1.pow(2.0) / 2.0 + &case1) + case2
+    }
+
+    // fn normalize_channels(&self) -> Tensor {
+    //     todo!();
+    // }
+
+    // fn normalize_channels_softmax(&self) -> Tensor {
+    //     todo!();
+    // }
 }
