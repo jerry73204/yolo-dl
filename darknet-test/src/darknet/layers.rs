66caab58236db8a685dbf684de582008f08b319c
jerry73204 <jerry73204@gmail.com>
2020-12-14 20:43:10 +0800

    Rewrite layer type and expose its output

--- darknet-test/src/darknet/layers.rs
+++ darknet-test/src/darknet/layers.rs
@@ -3,71 +3,72 @@ use super::kinds::{
 };
 use crate::{common::*, sys};
 
-/// A collection layers.
-#[derive(Debug)]
-pub struct Layers<'a> {
-    pub(crate) layers: &'a [sys::layer],
-}
+// /// A collection layers.
+// #[derive(Debug)]
+// pub struct Layers<'a> {
+//     pub(crate) layers: &'a [sys::layer],
+// }
 
-impl<'a> Layers<'a> {
-    /// Get the layer by index.
-    pub fn get(&self, index: usize) -> Option<Layer<'a>> {
-        self.layers.get(index).map(|layer| Layer { layer })
-    }
+// impl<'a> Layers<'a> {
+//     /// Get the layer by index.
+//     pub fn get(&self, index: usize) -> Option<LayerRef<'a>> {
+//         self.layers.get(index).map(|layer| LayerRef { layer })
+//     }
 
-    /// Get the iterator of the collection of layers.
-    pub fn iter(&'a self) -> LayersIter<'a> {
-        LayersIter {
-            layers: self,
-            index: 0,
-        }
-    }
-}
+//     /// Get the iterator of the collection of layers.
+//     pub fn iter(&'a self) -> LayersIter<'a> {
+//         LayersIter {
+//             layers: self,
+//             index: 0,
+//         }
+//     }
+// }
 
-impl<'a> IntoIterator for &'a Layers<'a> {
-    type IntoIter = LayersIter<'a>;
-    type Item = Layer<'a>;
+// impl<'a> IntoIterator for &'a Layers<'a> {
+//     type IntoIter = LayersIter<'a>;
+//     type Item = LayerRef<'a>;
 
-    fn into_iter(self) -> Self::IntoIter {
-        self.iter()
-    }
-}
+//     fn into_iter(self) -> Self::IntoIter {
+//         self.iter()
+//     }
+// }
 
-/// A iterator of a collection of layers.
-#[derive(Debug, Clone)]
-pub struct LayersIter<'a> {
-    layers: &'a Layers<'a>,
-    index: usize,
-}
+// /// A iterator of a collection of layers.
+// #[derive(Debug, Clone)]
+// pub struct LayersIter<'a> {
+//     layers: &'a Layers<'a>,
+//     index: usize,
+// }
 
-impl<'a> Iterator for LayersIter<'a> {
-    type Item = Layer<'a>;
+// impl<'a> Iterator for LayersIter<'a> {
+//     type Item = LayerRef<'a>;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let opt = self.layers.get(self.index);
-        if let Some(_) = opt {
-            self.index += 1;
-        }
-        opt
-    }
+//     fn next(&mut self) -> Option<Self::Item> {
+//         let opt = self.layers.get(self.index);
+//         if let Some(_) = opt {
+//             self.index += 1;
+//         }
+//         opt
+//     }
 
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        let len = self.layers.layers.len();
-        (len, Some(len))
-    }
-}
+//     fn size_hint(&self) -> (usize, Option<usize>) {
+//         let len = self.layers.layers.len();
+//         (len, Some(len))
+//     }
+// }
 
-impl<'a> FusedIterator for LayersIter<'a> {}
+// impl<'a> FusedIterator for LayersIter<'a> {}
 
-impl<'a> ExactSizeIterator for LayersIter<'a> {}
+// impl<'a> ExactSizeIterator for LayersIter<'a> {}
 
 /// A layer of the network.
 #[derive(Debug)]
-pub struct Layer<'a> {
-    pub(crate) layer: &'a sys::layer,
+#[repr(transparent)]
+pub struct Layer {
+    pub(crate) layer: sys::layer,
 }
 
-impl<'a> Layer<'a> {
+impl Layer {
     /// Get the type of layer.
     pub fn type_(&self) -> Option<LayerType> {
         FromPrimitive::from_usize(self.layer.type_ as usize)
@@ -159,5 +160,35 @@ impl<'a> Layer<'a> {
             self.output_height(),
             self.output_channels(),
         )
+    }
+
+    pub fn batch(&self) -> usize {
+        self.layer.batch as usize
+    }
+
+    pub fn output_slice(&self) -> &[f32] {
+        unsafe {
+            let batch = self.batch();
+            let (out_w, out_h, out_c) = self.output_shape();
+            let output_size = batch * out_w * out_h * out_c;
+            let slice = slice::from_raw_parts_mut(self.output, output_size);
+            slice
+        }
+    }
+
+    pub fn output_array(&self) -> ArrayView4<'_, f32> {
+        unsafe {
+            let batch = self.batch();
+            let (out_w, out_h, out_c) = self.output_shape();
+            ArrayView4::from_shape_ptr((batch, out_w, out_h, out_c), self.output)
+        }
+    }
+}
+
+impl Deref for Layer {
+    type Target = sys::layer;
+
+    fn deref(&self) -> &Self::Target {
+        &self.layer
     }
 }
