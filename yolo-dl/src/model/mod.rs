e2fb6ae7761657935792ec6b33a8fac6b39cfa07
jerry73204 <jerry73204@gmail.com>
2020-12-16 15:12:14 +0800

    Remove unused test

--- yolo-dl/src/model/mod.rs
+++ yolo-dl/src/model/mod.rs
@@ -24,46 +24,4 @@ mod tests {
         assert_eq!(init, recovered);
         Ok(())
     }
-
-    #[test]
-    fn yolo_v5_small_test() {
-        let device = Device::cuda_if_available();
-        let vs = nn::VarStore::new(device);
-        let root = vs.root();
-        let input_channels = 3;
-        let num_classes = 80;
-
-        let mut yolo_fn = yolo_v5_small(&root, input_channels, num_classes);
-
-        for _ in 0..10 {
-            let input = Tensor::randn(
-                &[32, input_channels as i64, 224, 224],
-                (Kind::Float, Device::cuda_if_available()),
-            );
-            let instant = std::time::Instant::now();
-            let mut output = yolo_fn.forward_t(&input, false);
-            // let detections = output.detections();
-            // let feature_maps = output.feature_maps();
-
-            // let feature_map_shapes = feature_maps
-            //     .iter()
-            //     .map(|tensor| tensor.size())
-            //     .collect::<Vec<_>>();
-            // println!("train output shapes: {:?}", feature_map_shapes);
-
-            // {
-            //     let expect = feature_map_shapes
-            //         .iter()
-            //         .map(|shape| match shape.as_slice() {
-            //             &[_bsize, channels, height, width, _outputs] => channels * height * width,
-            //             _ => unreachable!(),
-            //         })
-            //         .sum::<i64>();
-
-            //     assert_eq!(detections.len(), expect as usize);
-            // }
-
-            // println!("elapsed {:?}", instant.elapsed());
-        }
-    }
 }
