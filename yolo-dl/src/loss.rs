3858d355c24e3ca9bb773ec5d3afd4dcf159ea63
jerry73204 <jerry73204@gmail.com>
2021-01-25 20:10:02 +0800

    More debug assertions

--- yolo-dl/src/loss.rs
+++ yolo-dl/src/loss.rs
@@ -708,7 +708,15 @@ mod multi_bce_with_logit_loss {
     impl MultiBceWithLogitsLoss {
         pub fn forward(&self, input: &Tensor, target: &Tensor) -> Tensor {
             // assume [batch_size, n_classes] shape
-            debug_assert_eq!(input.size2().unwrap(), target.size2().unwrap());
+            debug_assert_eq!(
+                input.size2().unwrap(),
+                target.size2().unwrap(),
+                "input and target tensors must have equal shape"
+            );
+            debug_assert!(
+                bool::from(target.ge(0.0).logical_and(&target.le(1.0)).all()),
+                "target values must be in range of [0.0, 1.0]"
+            );
 
             // return zero tensor if (1) input is empty and (2) using mean reduction
             if input.is_empty() && self.reduction == Reduction::Mean {
@@ -787,6 +795,10 @@ mod focal_loss {
                 input.size2().unwrap(),
                 target.size2().unwrap(),
                 "input and target shape must be equal"
+            );
+            debug_assert!(
+                bool::from(target.ge(0.0).logical_and(&target.le(1.0)).all()),
+                "target values must be in range of [0.0, 1.0]"
             );
 
             // return zero tensor if (1) input is empty and (2) using mean reduction
