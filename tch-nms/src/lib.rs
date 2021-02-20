0e135f86dd8f477585c5e99e5da14c88b6457e02
jerry73204 <jerry73204@gmail.com>
2021-02-20 20:25:09 +0800

    Add nms_cuda_with_scores() and write tests

--- tch-nms/src/lib.rs
+++ tch-nms/src/lib.rs
@@ -61,6 +61,16 @@ pub fn nms_cuda_with_idx(
     }
 }
 
+pub fn nms_cuda_with_scores(
+    boxes: &Tensor,
+    scores: &Tensor,
+    nms_overlap_thresh: f64,
+    top_k: i64,
+) -> Result<(Tensor, Tensor, Tensor), TchError> {
+    let (_sorted, idx) = scores.sort(0, true);
+    nms_cuda_with_idx(boxes, &idx, nms_overlap_thresh, top_k)
+}
+
 pub(crate) fn read_and_clean_error() -> Result<(), TchError> {
     unsafe {
         match ptr_to_string(crate::get_and_reset_last_err()) {
@@ -90,24 +100,36 @@ mod tests {
         const N_BOXES: i64 = 1000;
         let device = Device::Cuda(0);
 
-        for _ in 0..10 {
-            let cy = Tensor::rand(&[N_BOXES, 1], (Kind::Float, device));
-            let cx = Tensor::rand(&[N_BOXES, 1], (Kind::Float, device));
-            let h = Tensor::rand(&[N_BOXES, 1], (Kind::Float, device));
-            let w = Tensor::rand(&[N_BOXES, 1], (Kind::Float, device));
+        let cy = Tensor::rand(&[N_BOXES, 1], (Kind::Float, device));
+        let cx = Tensor::rand(&[N_BOXES, 1], (Kind::Float, device));
+        let h = Tensor::rand(&[N_BOXES, 1], (Kind::Float, device));
+        let w = Tensor::rand(&[N_BOXES, 1], (Kind::Float, device));
 
-            let t = &cy - &h / 2.0;
-            let b = &cy + &h / 2.0;
-            let l = &cx - &w / 2.0;
-            let r = &cx + &w / 2.0;
+        let t = &cy - &h / 2.0;
+        let b = &cy + &h / 2.0;
+        let l = &cx - &w / 2.0;
+        let r = &cx + &w / 2.0;
 
-            let boxes = Tensor::cat(&[l, t, r, b], 1);
-            let idx = Tensor::full(&[N_BOXES], 0, (Kind::Float, device));
+        let boxes = Tensor::cat(&[l, t, r, b], 1);
+        let scores = Tensor::rand(&[N_BOXES], (Kind::Float, device));
 
-            let (_keep, _num_to_keep, _parent_object_index) =
-                nms_cuda_forward(&boxes, &idx, 0.98, 1).unwrap();
+        let (keep, num_to_keep, parent_object_index) =
+            nms_cuda_with_scores(&boxes, &scores, 0.50, N_BOXES).unwrap();
 
-            // dbg!(&keep, &num_to_keep, &parent_object_index);
-        }
+        assert!(keep.size1().unwrap() == N_BOXES);
+        assert!(parent_object_index.size1().unwrap() == N_BOXES);
+        assert!(num_to_keep.size() == vec![]);
+
+        let keep = Vec::<i64>::from(&keep);
+        let num_to_keep = i64::from(&num_to_keep);
+        let parent_object_index = Vec::<i64>::from(&parent_object_index);
+
+        assert!(keep[0..(num_to_keep as usize)]
+            .iter()
+            .all(|&val| val < N_BOXES));
+        assert!(keep[(num_to_keep as usize)..(N_BOXES as usize)]
+            .iter()
+            .all(|&val| val == 0));
+        assert!(parent_object_index.iter().all(|&val| val < N_BOXES));
     }
 }
