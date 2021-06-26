063bcc3c5b83db1f2c0966222a088c90767f6c9e
jerry73204 <jerry73204@gmail.com>
2021-06-26 10:08:03 +0800

    Simplify L2-norm impl

--- tch-modules/src/l2_loss.rs
+++ tch-modules/src/l2_loss.rs
@@ -11,18 +11,6 @@ impl L2Loss {
     }
 
     pub fn forward(&self, input: &Tensor, target: &Tensor) -> Tensor {
-        // return zero tensor if (1) input is empty and (2) using mean reduction
-        if input.is_empty() && self.reduction == Reduction::Mean {
-            return Tensor::zeros(&[], (Kind::Float, input.device())).set_requires_grad(false);
-        }
-
-        let loss = (input - target).pow(2);
-
-        match self.reduction {
-            Reduction::None => loss,
-            Reduction::Sum => loss.sum(Kind::Float),
-            Reduction::Mean => loss.mean(Kind::Float),
-            Reduction::Other(_) => unimplemented!(),
-        }
+        input.mse_loss(target, self.reduction)
     }
 }
