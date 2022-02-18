4f3bda955ed609ab2a730c798de45b83ad73132e
aeon <jerry73204@gmail.com>
2022-02-18 08:11:31 +0800

    Add add/sub/mu/div/rem ops on referenced unit wrappers

--- unit-wrapper/src/lib.rs
+++ unit-wrapper/src/lib.rs
@@ -6,6 +6,32 @@ macro_rules! unit_wrapper {
         #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
         $($vis)* struct $name<T>(pub T);
 
+        impl<T> $name<T> {
+            pub fn as_ref(&self) -> Pixel<&T> {
+                Pixel(&self.0)
+            }
+
+            pub fn as_mut(&mut self) -> Pixel<&mut T> {
+                Pixel(&mut self.0)
+            }
+
+            pub fn as_deref(&self) -> Pixel<&<T as std::ops::Deref>::Target>
+            where
+                T: std::ops::Deref
+            {
+                use std::ops::Deref;
+                Pixel(self.deref())
+            }
+
+            pub fn as_deref_mut(&mut self) -> Pixel<&mut <T as std::ops::Deref>::Target>
+            where
+                T: std::ops::DerefMut
+            {
+                use std::ops::DerefMut;
+                Pixel(self.deref_mut())
+            }
+        }
+
         impl<T> From<T> for $name<T> {
             fn from(value: T) -> Self {
                 Self(value)
@@ -38,6 +64,20 @@ macro_rules! unit_wrapper {
             }
         }
 
+        impl<'a, L, R> std::ops::Add<&'a $name<R>> for &'a $name<L>
+        where
+            L: 'a,
+            R: 'a,
+            &'a L: std::ops::Add<&'a R>,
+        {
+            type Output = $name<<&'a L as std::ops::Add<&'a R>>::Output>;
+
+            fn add(self, rhs: &'a $name<R>) -> Self::Output {
+                use std::ops::Add;
+                $name((&self.0).add(&rhs.0))
+            }
+        }
+
         impl<L, R> std::ops::Sub<$name<R>> for $name<L>
         where
             L: std::ops::Sub<R>,
@@ -49,6 +89,22 @@ macro_rules! unit_wrapper {
                 $name(self.0.sub(rhs.0))
             }
         }
+
+
+        impl<'a, L, R> std::ops::Sub<&'a $name<R>> for &'a $name<L>
+        where
+            L: 'a,
+            R: 'a,
+            &'a L: std::ops::Sub<&'a R>,
+        {
+            type Output = $name<<&'a L as std::ops::Sub<&'a R>>::Output>;
+
+            fn sub(self, rhs: &'a $name<R>) -> Self::Output {
+                use std::ops::Sub;
+                $name((&self.0).sub(&rhs.0))
+            }
+        }
+
 
         impl<L, R> std::ops::Mul<$name<R>> for $name<L>
         where
@@ -62,15 +118,45 @@ macro_rules! unit_wrapper {
             }
         }
 
-        impl<L, R> std::ops::Div<$name<R>> for $name<L>
-        where
-            L: std::ops::Div<R>,
-        {
-            type Output = $name<<L as std::ops::Div<R>>::Output>;
 
-            fn div(self, rhs: $name<R>) -> Self::Output {
-                use std::ops::Div;
-                $name(self.0.div(rhs.0))
+        impl<'a, L, R> std::ops::Mul<&'a $name<R>> for &'a $name<L>
+        where
+            L: 'a,
+            R: 'a,
+            &'a L: std::ops::Mul<&'a R>,
+        {
+            type Output = $name<<&'a L as std::ops::Mul<&'a R>>::Output>;
+
+            fn mul(self, rhs: &'a $name<R>) -> Self::Output {
+                use std::ops::Mul;
+                $name((&self.0).mul(&rhs.0))
+            }
+        }
+
+
+        impl<L, R> std::ops::Rem<$name<R>> for $name<L>
+        where
+            L: std::ops::Rem<R>,
+        {
+            type Output = $name<<L as std::ops::Rem<R>>::Output>;
+
+            fn rem(self, rhs: $name<R>) -> Self::Output {
+                use std::ops::Rem;
+                $name(self.0.rem(rhs.0))
+            }
+        }
+
+        impl<'a, L, R> std::ops::Rem<&'a $name<R>> for &'a $name<L>
+        where
+            L: 'a,
+            R: 'a,
+            &'a L: std::ops::Rem<&'a R>,
+        {
+            type Output = $name<<&'a L as std::ops::Rem<&'a R>>::Output>;
+
+            fn rem(self, rhs: &'a $name<R>) -> Self::Output {
+                use std::ops::Rem;
+                $name((&self.0).rem(&rhs.0))
             }
         }
     };
